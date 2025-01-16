06d6397457739f055295eafb98c89a68a267a8d6
FelixGCliche <felix.gagne.cliche@gmail.com>
2025-01-16 17:03:18 -0500

    add default plugin settings in project settings window

---
+++ addons/enet-multiplayer-service/config/config_loader.gd
@@ -0,0 +1,62 @@
+class_name ENetMultiplayerConfigLoader
+extends RefCounted
+
+const CONFIG_SECTION: StringName = "enet_multiplayer_service"
+
+const DEFAULT_PLUGIN_SETTINGS: Dictionary = {
+  "default_server_address": "localhost",
+  "default_server_port": 31401,
+  "max_clients": 32,
+  "allow_dedicated_server": true,
+  "allow_upnp": true
+}
+# gdlint: disable=max-line-length
+const PLUGIN_SETTING_INFO: Dictionary = {
+  "default_server_address":
+  "The default address used to connect to the server.",
+  "default_server_port":
+  "The default port the host listens to when creating a server",
+  "max_clients":
+  "The maximum number of clients allowed to connect to the server at once.",
+  "use_dedicated_server":
+  "Allows the host to be run on a [url=https://docs.godotengine.org/en/stable/tutorials/export/exporting_for_dedicated_servers.html]dedicated server[/url].",
+  "use_upnp": "Allows the use of UPNP to manage remote server setup."
+}
+# gdlint: enable=max-line-length
+
+
+static func add_custom_project_settings(
+  name: StringName,
+  default_value: Variant,
+  type: int,
+  hint: int = PROPERTY_HINT_NONE
+) -> void:
+  if ProjectSettings.has_setting(name):
+    return
+  ProjectSettings.set_setting(name, default_value)
+  ProjectSettings.set_initial_value(name, default_value)
+  ProjectSettings.set_as_basic(name, true)
+
+  if !PLUGIN_SETTING_INFO.has(name):
+    return
+  var property_info: Dictionary = {
+    "name": name,
+    "type": type,
+    "hint": hint,
+    "hint_string": PLUGIN_SETTING_INFO[name]
+  }
+  ProjectSettings.add_property_info(property_info)
+
+
+static func init_default_plugin_settings() -> void:
+  for setting_name in DEFAULT_PLUGIN_SETTINGS:
+    var setting_value: Variant = DEFAULT_PLUGIN_SETTINGS[setting_name]
+    add_custom_project_settings(
+      _get_project_setting_path(setting_name),
+      setting_value,
+      typeof(setting_value)
+    )
+
+
+static func _get_project_setting_path(name: StringName) -> StringName:
+  return "%s/%s" % [CONFIG_SECTION, name]
--- addons/enet-multiplayer-service/plugin.gd
+++ addons/enet-multiplayer-service/plugin.gd
@@ -3,6 +3,7 @@ class_name ENetMultiplayerServicePlugin
 extends EditorPlugin
 
 const AUTOLOAD_NAME: StringName = "ENetMultiplayerService"
+#gdlint: disable=max-line-length
 const AUTOLOAD_PATH: StringName = "res://addons/enet-multiplayer-service/enet_multiplayer_service.tscn"
 const AUTOLOAD_OPTIONS_PATH: StringName = "autoload/" + AUTOLOAD_NAME
 
@@ -12,13 +13,16 @@ func _enable_plugin() -> void:
     add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
     print("%s enabled " % AUTOLOAD_NAME)
 
+
 func _disable_plugin() -> void:
   if ProjectSettings.has_setting(AUTOLOAD_OPTIONS_PATH):
     remove_autoload_singleton(AUTOLOAD_NAME)
     print("%s disabled " % AUTOLOAD_NAME)
 
+
 func _enter_tree() -> void:
   print("%s initialized " % AUTOLOAD_NAME)
+  ENetMultiplayerConfigLoader.init_default_plugin_settings()
 
 
 func _exit_tree() -> void:
