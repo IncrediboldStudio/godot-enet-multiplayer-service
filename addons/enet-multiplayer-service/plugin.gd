62060195422e28b7b3bd6af15e87d0f63fa05983
FelixGCliche <felix.gagne.cliche@gmail.com>
2025-01-16 13:04:05 -0500

    add autoload initialization

--- addons/enet-multiplayer-service/plugin.gd
+++ addons/enet-multiplayer-service/plugin.gd
@@ -1,12 +1,25 @@
 @tool
+class_name ENetMultiplayerServicePlugin
 extends EditorPlugin
 
+const AUTOLOAD_NAME: StringName = "ENetMultiplayerService"
+const AUTOLOAD_PATH: StringName = "res://addons/enet-multiplayer-service/enet_multiplayer_service.tscn"
+const AUTOLOAD_OPTIONS_PATH: StringName = "autoload/" + AUTOLOAD_NAME
+
+
+func _enable_plugin() -> void:
+  if !ProjectSettings.has_setting(AUTOLOAD_OPTIONS_PATH):
+    add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
+    print("%s enabled " % AUTOLOAD_NAME)
+
+func _disable_plugin() -> void:
+  if ProjectSettings.has_setting(AUTOLOAD_OPTIONS_PATH):
+    remove_autoload_singleton(AUTOLOAD_NAME)
+    print("%s disabled " % AUTOLOAD_NAME)
 
 func _enter_tree() -> void:
-  # Initialization of the plugin goes here.
-  pass
+  print("%s initialized " % AUTOLOAD_NAME)
 
 
 func _exit_tree() -> void:
-  # Clean-up of the plugin goes here.
   pass
